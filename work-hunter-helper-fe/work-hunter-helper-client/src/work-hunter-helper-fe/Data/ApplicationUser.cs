5b20887cad9c19c857fa03de5bfc5c69a0ce1e49
Олег Летуновский <letunovskij.oleg@gmail.com>
2025-03-18 08:38:34 +0300

    #00 initial commit

---
+++ work-hunter-helper-fe/work-hunter-helper-client/src/work-hunter-helper-fe/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace work_hunter_helper_fe.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
